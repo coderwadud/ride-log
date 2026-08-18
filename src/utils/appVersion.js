b566a98b1154060fb69a0ac7789a1888fb45e246
wadud.hossain <coder.wadud@gmail.com>
2026-08-18 16:36:00 +0600

    v1.6.2: Upgrade app version to 1.6.2 (build.gradle versionCode 17)

--- src/utils/appVersion.js
+++ src/utils/appVersion.js
@@ -1,7 +1,7 @@
 import { App } from '@capacitor/app';
 import { Capacitor } from '@capacitor/core';
 
-export const BASE_APP_VERSION = '1.6.1';
+export const BASE_APP_VERSION = '1.6.2';
 
 /**
  * Dynamically gets the actual installed APK version from Android OS,
